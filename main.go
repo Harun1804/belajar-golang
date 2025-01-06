ab9ec06b21992c33495117a3ebe9c843d4dae776
Harun <Harun.arrasyid1804@gmail.com>
2025-01-06 16:12:24 +0700

    Channel

--- main.go
+++ main.go
@@ -8,23 +8,30 @@ import (
 // import "first-app/interfaces"
 // import "first-app/geometry"
 
-func print(till int, message string) {
-	for i := 0; i < till; i++ {
-		fmt.Println((i + 1), message)
-	}
-}
-
-
 func main() {
 	runtime.GOMAXPROCS(2)
 
 	// messages := make(chan int, 2)
 
-	go print(5, "halo")
-	print(5, "apa kabar")
+	var messages = make(chan string)
 
-	var input string
-	fmt.Scanln(&input)
+	var sayHelloTo = func (who string)  {
+		var data = fmt.Sprintf("hello %s", who)
+		messages <- data
+	}
+
+	go sayHelloTo("john wick")
+	go sayHelloTo("ethan hunt")
+	go sayHelloTo("jason bourne")
+
+	var message1 = <-messages
+	fmt.Println(message1)
+
+	var message2 = <-messages
+	fmt.Println(message2)
+
+	var message3 = <-messages
+	fmt.Println(message3)
 
 	// go func ()  {
 	// 	for {
