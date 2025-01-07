5c7d5fdf4ba32aafa52fc0c95b67f04370d6390b
Harun <Harun.arrasyid1804@gmail.com>
2025-01-07 08:26:13 +0700

    Select Channel

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"first-app/calculate"
 )
 
 // import "first-app/interfaces"
@@ -11,19 +12,36 @@ import (
 func main() {
 	runtime.GOMAXPROCS(2)
 
-	messages := make(chan int, 2)
+	var numbers = []int{3, 4, 3, 5, 2, 3, 5, 6, 7, 4, 3, 2, 4, 5, 3, 2, 4, 6, 7, 8, 9, 0, 8, 7, 6, 5, 4, 3, 2, 1}
 
-	go func ()  {
-		for {
-			i := <- messages
-			fmt.Println("receive data", i)
+	var ch1 = make(chan float64)
+	go calculate.GetAverage(numbers, ch1)
+
+	var ch2 = make(chan int)
+	go calculate.GetMax(numbers, ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+			case avg := <-ch1:
+				fmt.Printf("Average \t: %.2f\n", avg)
+			case max := <-ch2:
+				fmt.Printf("Max \t: %d\n", max)
 		}
-	}()
-
-	for i := 0; i < 5; i++ {
-		fmt.Println("send data", i)
-		messages <- i
 	}
+
+	// messages := make(chan int, 2)
+
+	// go func ()  {
+	// 	for {
+	// 		i := <- messages
+	// 		fmt.Println("receive data", i)
+	// 	}
+	// }()
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("send data", i)
+	// 	messages <- i
+	// }
 
 	// var bangunDatar interfaces.Hitung
 	// bangunDatar = geometry.Persegi{Sisi: 10.0}
