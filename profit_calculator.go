8b2a2244aa2c6d90e047d11af84b447c08dc1394
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-04-08 14:24:45 +0700

    Excercise

--- profit_calculator.go
+++ profit_calculator.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main()  {
-	revenue := getUserInput("Enter revenue: ")
-	expenses := getUserInput("Enter expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, errRevenue := getUserInput("Enter revenue: ")
+	expenses, errExpenses := getUserInput("Enter expenses: ")
+	taxRate, errTax := getUserInput("Enter tax rate: ")
+
+	if errRevenue != nil {
+		showError(errRevenue)		
+	}
+
+	if errExpenses != nil {
+		showError(errExpenses)		
+	}
+
+	if errTax != nil {
+		showError(errTax)		
+	}
 
 	ebt, profit, ratio := calculateProfit(revenue, expenses, taxRate)
 
+	writeIntoFile(ebt, profit, ratio)
 	formattedOutput("EBT", ebt)
 	formattedOutput("Profit", profit)
 	formattedOutput("Profit ratio", ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var input float64
+
 	fmt.Print(infoText)
 	fmt.Scan(&input)
-	return input
+
+	if input <= 0 {
+		return 0, fmt.Errorf("%s input must be greater than zero", infoText)
+	}
+
+	return input, nil
 }
 
 func calculateProfit(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
@@ -32,4 +52,29 @@ func calculateProfit(revenue, expenses, taxRate float64) (ebt float64, profit fl
 
 func formattedOutput(label string, value float64)  {
 	fmt.Printf("%s = %.2f\n", label, value)
+}
+
+func showError(err error) {
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("---------------------------")
+		return
+	}
+}
+
+func writeIntoFile(ebt, profit, ratio float64) {
+	file, err := os.Create("profit.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "Ebt: %.2f\nProfit: %.2f\nRatio Rate: %.2f\n", ebt, profit, ratio)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("Data written to profit.txt")
 }
