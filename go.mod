6ae56db5c09853f828b7e02dc2b95deeccda5507
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-04-21 13:44:26 +0700

    struct data type

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example.com/pointers
+module example.com/struct
 
 go 1.23.3
---
+++ structs.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Enter your first name:")
+	userLastName := getUserData("Enter your last name:")
+	userBirthdate := getUserData("Enter your birth date (YYYY-MM-DD):")
+
+	userData := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	outputUserData(userData)
+}
+
+func outputUserData(u user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Println(promptText)
+	var input string
+	fmt.Scanln(&input)
+	return input
+}
