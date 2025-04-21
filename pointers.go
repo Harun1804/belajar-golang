2954b16a19abe30b57b98d3923d27943fbda08c7
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-04-21 10:53:17 +0700

    add pointer as variabel function

--- pointers.go
+++ pointers.go
@@ -8,4 +8,11 @@ func main() {
 	fmt.Println("Age:", age) // prints 32
 	fmt.Println("Age pointer:", agePtr) // prints the address of age variable
 	fmt.Println("Age pointer value:", *agePtr) // prints the value of age variable using pointer
+
+	adultYear := getAdultYear(agePtr) // passing the address of age variable to the function
+	fmt.Println("Adult year:", adultYear) // prints 14 (32 - 18)
+}
+
+func getAdultYear(age *int) int {
+	return *age - 18
 }
