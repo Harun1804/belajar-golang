cba9b2fde0c916f95a2a1e0b6e2c8933e5fade48
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-06-03 11:14:41 +0700

    spliting split into param

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 func main() {
+	numbers := []int{1, 2, 3, 4, 5}
 	number:= sumup(1, 2, 3, 4, 5)
+	anotherNumber := sumup(numbers...)
 	println("Sum of numbers is:", number)
+	println("Sum of another numbers is:", anotherNumber)
 }
 
 func sumup(numbers ...int) int {
