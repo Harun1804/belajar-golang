bcdd6853a0b865ccab8e8174f8a2cf8f514f779b
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-06-03 11:09:08 +0700

    add variadic function

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
 func main() {
-	
+	number:= sumup(1, 2, 3, 4, 5)
+	println("Sum of numbers is:", number)
+}
+
+func sumup(numbers ...int) int {
+	result := 0
+	for _, number := range numbers {
+		result += number
+	}
+
+	return result
 }
