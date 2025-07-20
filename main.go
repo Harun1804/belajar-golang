99f502cea59d9fbab734985ec35aa006d461b7fa
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-20 19:07:39 +0700

    add hello world

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main()  {
-	
+	server := gin.Default()
+
+	server.GET("/ping", ping)
+
+	server.Run(":8080")
+}
+
+func ping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": true,
+		"message": "pong",
+		"data": nil,
+	})
 }
