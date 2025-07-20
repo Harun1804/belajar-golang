cd85b02826c6b0a0c7d7707f312cef532fe0370f
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-20 20:08:42 +0700

    Add model and post request

--- main.go
+++ main.go
@@ -3,21 +3,47 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main()  {
 	server := gin.Default()
 
-	server.GET("/ping", ping)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
-func ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
+func getEvents(context *gin.Context) {
+	events := models.GetEvents()
+	context.JSON(http.StatusOK, gin.H{
 		"status": true,
-		"message": "pong",
-		"data": nil,
+		"message": "Events retrieved successfully",
+		"data": events,
+	})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": "Invalid input data",
+			"data":    nil,
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1 // Assuming a static UserID for simplicity
+
+	context.JSON(http.StatusCreated, gin.H{
+		"status":  true,
+		"message": "Event created successfully",
+		"data":    event,
 	})
 }
