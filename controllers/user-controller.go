6d93dd9ae5c501f209674a3481a6d10ad0a43652
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-23 10:37:27 +0700

    add sign up user

---
+++ controllers/user-controller.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func StoreUser(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": "Invalid input data",
+			"data":    nil,
+		})
+		return
+	}
+
+	err = user.CreateUser()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"status":  false,
+			"message": "Failed to store user",
+			"data":    nil,
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"status":  true,
+		"message": "User created successfully",
+		"data":    user,
+	})
+}
